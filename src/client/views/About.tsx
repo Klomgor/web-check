83548b99060c175dafedf63424e8bf98b56721bc
Alicia Sykes <alicia@omg.lol>
2026-07-28 18:59:39 +0100

    Fixes google API link mistake (#63)

--- src/client/views/About.tsx
+++ src/client/views/About.tsx
@@ -393,11 +393,11 @@ const About = (): JSX.Element => {
               <a
                 target="_blank"
                 rel="noreferrer"
-                href="https://cloud.google.com/api-gateway/docs/authenticate-api-keys"
+                href="https://developers.google.com/speed/docs/insights/v5/get-started"
               >
                 A Google API key
               </a>
-              <i> Used to return quality metrics for a site</i>
+              <i> With the PageSpeed Insights API enabled, used to return quality metrics for a site</i>
             </li>
             <li>
               <code>REACT_APP_SHODAN_API_KEY</code>:{' '}
